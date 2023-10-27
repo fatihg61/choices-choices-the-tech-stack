811f889d9535e947d0dbf4d178d03c1b34ae86b9
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2023-10-27 16:14:01 +0200

    Routing done

--- ink/src/App.js
+++ ink/src/App.js
@@ -1,11 +1,21 @@
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './about.js'; // The new page component
+
+// import './main.css';
+
+export let data
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="./About.js" element={<About />} />
+      </Routes>
       {/* The rest of your app content goes here */}
       <div className="main-content">
         {/* Your app content */}
