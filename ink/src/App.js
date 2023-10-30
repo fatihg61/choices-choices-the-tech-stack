161c5ba63a2971a8bf899b6ff67bbbdcb9720849
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2023-10-30 13:22:17 +0100

    routing

--- ink/src/App.js
+++ ink/src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="./About.js" element={<About />} />
+        <Route path="./about.js" element={<About />} />
       </Routes>
       {/* The rest of your app content goes here */}
       <div className="main-content">
